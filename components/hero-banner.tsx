c167ac8699b2339a6b69d3a32d85c8ba4ca6a56c
Laurenz Guevara <laurenz.guevara@gmail.com>
2026-01-30 23:17:47 +0000

    HW-10: Create wireframe hero banner component

---
+++ components/hero-banner.tsx
@@ -0,0 +1,17 @@
+export function HeroBanner() {
+  return (
+    <section className="bg-blue-600">
+      <div className="container px-4 mx-auto md:gap-x-10 md:grid md:grid-cols-2 py-12 page-container">
+        <p className="text-2xl sm:text-3xl font-bold pb-5 md:pb-0">
+          Lorem ipsum dolor sit amet, consectetur.
+        </p>
+        <span className="flex items-center">
+          <p className="text-xl">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in neque sodales, pulvinar felis eget, iaculis lorem. Donec neque eget.
+          </p>
+        </span>
+      </div>
+    </section>
+  )
+}
+
